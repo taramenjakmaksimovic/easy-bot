c6ad6db378cd11554e7701d154bebdf3b1ee0e19
taramenjakmaksimovic <taramm19@gmail.com>
2026-04-17 22:40:10 +0200

    Update generativeai dependency to 0.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,5 +56,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation("com.google.ai.client.generativeai:generativeai:0.7.0")
+    implementation("com.google.ai.client.generativeai:generativeai:0.9.0")
 }
